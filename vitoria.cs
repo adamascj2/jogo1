3e3d792df88bb3cfbaec86777e0b36cabb119f5a
adamascj2 <adamascj@gmail.com>
2020-09-12 18:12:55 -0300

    Add files via upload

---
+++ vitoria.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+ 
+public class vitoria: MonoBehaviourPun
+{
+ AudioSource  MyAudioSource;
+   
+   void Start()
+   {
+     MyAudioSource = GetComponent<AudioSource>();
+     
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag == "robot"){
+        MyAudioSource.Play();
+      }
+     }
+}
+
+    
