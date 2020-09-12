f83ac717592848c1dae944b0340825a3f55e85ad
adamascj2 <adamascj@gmail.com>
2020-09-12 18:13:41 -0300

    Add files via upload

---
+++ Abreporta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abreporta : MonoBehaviour
+{
+  public static bool   temchave = false;
+  public static bool fechada = true;
+  public static bool tocou = false;
+   
+   
+     void Update()
+    {
+     GameObject GOP = GameObject.Find("porta2");
+       if(temchave &&  fechada && Vector3.Distance(transform.position,GOP.transform.position)<3 && GOP!=null){
+          GOP.transform.Translate(Vector3.right*2);//inutil pq destroy
+          fechada = false;
+          tocou = true;
+          Destroy(GOP,.5f);
+        }
+       //if(temchave && !fechada && Vector3.Distance(transform.position,GOP.transform.position)>6){
+         //GOP.transform.Translate(Vector3.right*-2);
+         //fechada = true;
+         //tocou = true;
+       //}//dava bug donde usei destroy
+       
+    } 
+    
+}
