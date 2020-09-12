a7200491b04bf2d398b8144846cfe718844dc37f
adamascj2 <adamascj@gmail.com>
2020-09-12 18:14:56 -0300

    Add files via upload

---
+++ somdeporta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class somdeporta : MonoBehaviour
+{
+AudioSource  MyAudioSource;
+     
+    void Start()
+    {
+      MyAudioSource = GetComponent<AudioSource>();
+    }
+
+     
+    void Update()
+    {
+      if(Abreporta.tocou){   
+       MyAudioSource.Play();
+       Abreporta.tocou= false;       
+      }     
+     }
+ 
+       
+}
