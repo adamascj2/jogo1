821ebe1ecb3e19990f82523775c6069674609684
adamascj2 <adamascj@gmail.com>
2020-09-12 18:12:31 -0300

    Add files via upload

---
+++ pegachave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+ 
+public class pegachave: MonoBehaviourPun
+{
+  AudioSource  MyAudioSource;
+   
+   void Start()
+   {
+      MyAudioSource = GetComponent<AudioSource>();
+   }
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+            
+      if(other.gameObject.tag == "robot"){
+        MyAudioSource.Play();
+        Abreporta.temchave=true;
+        yield return new WaitForSeconds(3);//pra tocar antes de destroy
+        Destroy(gameObject,.5f);
+            
+       }
+      }
+}
+
+    
